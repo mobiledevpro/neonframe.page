a4772fcf3aac415e9c2ddc92438e028275107687
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2024-01-07 21:41:30 +0200

    Updated Readme

--- _src/index.js
+++ _src/index.js
@@ -5,8 +5,8 @@ import { default as Tobi } from '@midzer/tobii'
 const tobi = new Tobi()
 
 // Remove the two following lines to remove the product hunt floating prompt
-import FloatingPrompt from 'producthunt-floating-prompt'
-FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
+//import FloatingPrompt from 'producthunt-floating-prompt'
+//FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
 
 // Remove the following lines to remove the darkmode js
 import Darkmode from 'darkmode-js'
